1071c163c3b819fc88c845db30c7318fda31772f
Hamzah Chaudhry <151885329+hamzahchaudhry@users.noreply.github.com>
2024-09-26 21:39:01 -0700

    email encrypred

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+var encEmail = "aGFtemFoY2hhdWRocnkzMjFAZ21haWwuY29t=";
+const form = document.getElementById("email");
+form.setAttribute("href", "mailto:".concat(atob(encEmail)));
